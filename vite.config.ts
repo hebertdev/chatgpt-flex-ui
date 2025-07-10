385b00b1c1bc36a545f97d0da41144005463cd81
hebertdev <hebert.villafuerte.c@hotmail.com>
2025-07-10 11:19:59 -0500

    dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 // vite.config.ts
-import { VitePWA } from "vite-plugin-pwa";
+// Eliminar esta importación
+// import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react-swc";
 import * as path from 'path'
 
@@ -7,47 +8,7 @@ import * as path from 'path'
 export default {
   plugins: [
     react(),
-    VitePWA({
-      // add this to cache all the imports
-      workbox: {
-        globPatterns: ["**/*"],
-      },
-      // add this to cache all the
-      // static assets in the public folder
-      includeAssets: ["**/*"],
-      manifest:{
-        theme_color: '#75ab9d',
-        background_color: '#75ab9d',
-        display: 'standalone',
-        scope: '/',
-        start_url: '/',
-        name: 'GPT Flex',
-        short_name: 'GPT Flex',
-        description: 'About ChatGPT-Flex-UI es una interfaz moderna y adaptable para la API de Chat GPT',
-        icons: [
-          {
-            src: '/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icon-256x256.png',
-            sizes: '256x256',
-            type: 'image/png',
-          },
-          {
-            src: '/icon-384x384.png',
-            sizes: '384x384',
-            type: 'image/png',
-          },
-          {
-            src: '/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      }
-    }),
+    // Eliminar toda la configuración de VitePWA
   ],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
